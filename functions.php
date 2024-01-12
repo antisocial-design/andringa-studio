0704e1a717ab74640362f2acd6a0346cd0b3cf91
antisocial-design <miguelmirandadesign@gmail.com>
2024-01-12 19:42:07 +0000

    update

--- functions.php
+++ functions.php
@@ -9,11 +9,11 @@ function register_menus()
     register_nav_menu('policy-menu', 'Policy Menu');
 }
 
-function theme_add_woocommerce_support()
-{
-    add_theme_support('woocommerce');
+function theme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
 }
-add_action('after_setup_theme', 'theme_add_woocommerce_support');
+
+add_action( 'after_setup_theme', 'theme_add_woocommerce_support' );
 
 // show_admin_bar(false);
 add_post_type_support('page', 'excerpt', 'post-thumbnails');
--- woocommerce.php
+++ woocommerce.php
@@ -1,32 +1,11 @@
 <?php
-defined('ABSPATH') || exit;
-$context = Timber::context();
+
+$context            = Timber::context();
 $context = init_timber();
-$context['post'] = new Timber\Post();
 
-if (is_product_category()) {
-    $queried_object = get_queried_object();
-    $term_id = $queried_object->term_id;
-    $context['category'] = get_term($term_id, 'product_cat');
-    $args = array(
-        'post_type' => 'product',
-        'posts_per_page' => -1,
-        'post_status' => 'publish',
-        'orderby' => 'date',
-        'order' => 'DESC',
-        'tax_query' => array(
-            array(
-                'taxonomy' => 'product_cat',
-                'field' => 'term_id',
-                'terms' => $term_id,
-            ),
-        ),
-    );
 
-    $context['products'] = Timber::get_posts($args);
-
-    Timber::render('templates/archive.twig', $context);
-} else if (is_singular('product')) {
+if ( is_singular( 'product' ) ) {
+    $context['post'] = new Timber\Post();
     $product = wc_get_product(get_the_ID());
 
     $args = array(
@@ -53,7 +32,38 @@ if (is_product_category()) {
     $context['related_items'] = $related_items;
     $context['archive_link'] = get_the_permalink(6);
 
+    wp_reset_postdata();
+
     Timber::render('single-product.twig', $context);
+} else {
+    $context['customTitle'] = $context['wp_title'];
+    
+    $posts = Timber::get_posts();
+    $context['products'] = $posts;
+    // $context['products'] = Timber::get_posts($args);
+
+    if ( is_product_category() ) {
+        $queried_object = get_queried_object();
+    $term_id = $queried_object->term_id;
+    $context['category'] = get_term($term_id, 'product_cat');
+    $args = array(
+        'post_type' => 'product',
+        'posts_per_page' => -1,
+        'post_status' => 'publish',
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'product_cat',
+                'field' => 'term_id',
+                'terms' => $term_id,
+            ),
+        ),
+    );
+
+
 }
 
-
+Timber::render('templates/archive.twig', $context);
+    // Timber::render( 'views/woo/archive.twig', $context );
+}
